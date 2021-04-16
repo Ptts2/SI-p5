a555fa885d370b58a2072fc1b1a909f3942fd6e4
Ptts2 <47633569+Ptts2@users.noreply.github.com>
2021-04-16 10:50:03 +0200

    Funciona

--- p5.py
+++ p5.py
@@ -5,27 +5,35 @@ alf="aábcdeéfghiíjklmnñoópqrstuúvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚVWXY
 msj_cifrado="ÁeóÍ ebá 5b-CeóÍósUÍCs sÍ2UeÍÚLVVpt)utÍoáÍez2ehÍÍíN1mX-ñjA1E-OmimjX-wOyimj3wPFé13iAimÚj-mj31-OXwÚjF-OwjjbmYf2áUspY7ÍíPomY íYy3KYí ÚoPbbmEYÓ YP:3mbYyÁLÁYY4v6z6(znmsnzh(v:6zW6fW6zvoz(vóp-z6(6MpWÉzxpOFpzzÍ.íÍa3ñcahuiÍa.Í3uV Ía,ua úc.uVáúua3ñca5y(Zj9aa)r7NOFyWOwóyOÁNuukYóRYOKyRYKdRkÁy(OOIiPúGTókCF5yaó95FCyCsaTó)aQAQóiZGZ("
 
 n_simb = len(alf) #modulo
-c_descifrado = [64, 5]
 lineas = []
 
-for i in range(0, 3):
-    print(i)
+#decodificacion
 
-#decodificacion con clave inicial
+#a y b iniciales
+ab = [64, 5]
+p = 1
+a = pow(ab[0], p, n_simb)
+b = (ab[1]*p)
 
-i = 0;
 linea = ""
-p = 1;
-c_des = pow(c_descifrado[0], -p, n_simb)
-for i in range(1, len(msj_cifrado)):
-    if(linea and linea[-1] ==" " and linea[-2] ==" "):
+
+for i in range(0, len(msj_cifrado)):
+
+    #Si es nueva linea
+    if(linea!="" and linea[-1] ==" " and linea[-2] ==" "):
+        #Recalculo la clave de cifrado
         p = p+1
-        c_des = pow(c_descifrado[0], -p, n_simb)
+        a = pow(ab[0], p, n_simb)
+        b = (ab[1]*p % n_simb)
+
+        #Lo añado a la lista de lineas y vacio la linea
         lineas.append(linea)
         linea = ""
-    
-    n = alf.index(msj_cifrado[i])
-    new_n = (c_descifrado[0]*n+c_descifrado[1])%n_simb #an + b
-    linea += alf[new_n]
-    
-print(lineas)
+
+    c = alf.index(msj_cifrado[i]) #Tomo la posicion en el alfabeto del caracter cifrado
+    new_n = ((pow(a, -1, n_simb)*c)%n_simb) - ((pow(a, -1, n_simb)*b)%n_simb)%n_simb #Calculo la nueva a^-1*c - a^-1*b
+    linea += alf[new_n] #Añado la letra descrifrada a la linea
+
+lineas.append(linea) #añado la ultima linea
+
+print(*lineas, sep = "\n")
